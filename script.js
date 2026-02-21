c185a3916ab69f2ef315eda1d61312f9d15d558e
hasan0v147 <abdugapparsaltanat@gmail.com>
2026-02-22 00:54:37 +0500

    First Commits

---
+++ script.js
@@ -0,0 +1,11 @@
+function goPage2() {
+  window.location.href = "page2.html";
+}
+
+function openCard() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closeCard() {
+  document.getElementById("popup").style.display = "none";
+}
